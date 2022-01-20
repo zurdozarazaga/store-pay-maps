40918c195b428f10e67521e64c1eb98ab747b7d2
zurdozarazaga <zurdozarazaga@gmail.com>
2022-01-20 12:08:11 -0300

    integracion completa de paypal

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,13 +1,70 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { PayPalButton } from 'react-paypal-button-v2';
+import { useNavigate } from 'react-router-dom';
+import AppContext from '../context/AppContext';
 import '../styles/components/Payment.css';
 
 const Payment = () => {
+  const { state, addNewOrder } = useContext(AppContext);
+  const { cart, buyer } = state;
+  const navigate = useNavigate();
+
+
+  const paypalOtions = {
+    clientId: process.env.CLIENT_ID_SB,
+    intent: 'capture',
+    currency: 'USD'
+  }
+
+  const buttonStyles = {
+    layout: 'vertical',
+    shape: 'rect'
+  }
+
+  const handlePaymentSuccess = (data) => {
+    console.log(data);
+    if (data.status === 'COMPLETED') {
+      const newOrder = {
+        buyer,
+        product: cart,
+        payment: data
+      }
+      addNewOrder(newOrder);
+      navigate('/checkout/success')
+    }
+  }
+
+  const handleSumTotal = () => {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
+    const sum = cart.reduce(reducer, 0);
+    return sum;
+  }
+
   return (
     <div className="Payment">
       <div className="Payment-content">
-        <h3>Resumen del pedido:</h3>
+        <h3>Resument del pedido:</h3>
+        {cart.map((item) => (
+          <div className="Payment-item" key={item.title}>
+            <div className="Payment-element">
+              <h4>{item.title}</h4>
+              <span>
+                $
+                {' '}
+                {item.price}
+              </span>
+            </div>
+          </div>
+        ))}
         <div className="Payment-button">
-          Boton de pago con Paypal
+        <PayPalButton
+            paypalOptions={paypalOtions}
+            buttonStyles={buttonStyles}
+            amount={handleSumTotal()}
+            onSuccess={data => handlePaymentSuccess(data)}
+            onError={error => console.log(error)}
+            onCancel={data => console.log(data)}
+          />
         </div>
       </div>
       <div />
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin').default;
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -48,6 +49,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'assets/[name].css',
     }),
+    new Dotenv(),
   ],
   devServer: {
     static: {
